9f78bafdf01fa6dd65b5ccdfc7ceb202253ca282
abdallamfathy <abdallamfathy@gmail.com>
2022-08-20 15:33:21 +0200

    Make fetching dynamic

--- main.js
+++ main.js
@@ -5,19 +5,23 @@ let showData = document.querySelector(".show-data span");
 let reposData = document.querySelector(".show-data");
 
 getButton.onclick = function () {
-    if (inputData.value === '') {
-        showData.innerHTML = "Please Enter repo's username!"
-    } else {
-        getData();
-        showData.innerHTML = ""
-         
-    }
+    getData();
 }
 
 function getData() {
-      fetch('https://api.github.com/users/hadley/orgs')
+    // Check if input is empety
+    if (inputData.value === '') {
+        // Add message to user
+        showData.innerHTML = "Please Enter repo's username!"
+    } else {
+        // fetching data
+      fetch(`https://api.github.com/users/${inputData.value}/repos`)
+      // get data in json
      .then((response) => response.json())
+     // return data
      .then((repos) => {
+        
+        showData.innerHTML = "";
         repos.forEach(repo => {
         // Create div
         let mainDiv = document.createElement("div");
@@ -38,13 +42,19 @@ function getData() {
         // Create stars span
         let starsSpan = document.createElement("span");
         // Create text for span
-        let starsSpanText = document.createTextNode(`Stars count is : ${stargazer_count}`);
+        let starsSpanText = document.createTextNode(`Stars count is : ${repo.stargazers_count}`);
         // Add text to span
         starsSpan.appendChild(starsSpanText);
         // Add stars to mainDiv
         mainDiv.appendChild(starsSpan);
+        // Add Class On Main Div
+        mainDiv.className = 'repo-box';
+        // Append The Main Div To Container
+        reposData.appendChild(mainDiv);
         });
         
      });
+        
+    }
      
 }
