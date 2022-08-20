7f407281055280950753122c41912e0fbb11ab69
abdallamfathy <abdallamfathy@gmail.com>
2022-08-20 14:38:55 +0200

    make function for fetching data

--- main.js
+++ main.js
@@ -4,3 +4,19 @@ let getButton = document.querySelector(".get-button");
 let showData = document.querySelector(".show-data span");
 let showDataFetched = document.querySelector(".show-data");
 
+getButton.onclick = function () {
+    if (inputData.value === '') {
+        showData.innerHTML = "Please Enter repo's username!"
+    } else {
+        getData();
+        
+         
+    }
+}
+
+function getData() {
+      fetch('https://api.github.com/users/hadley/orgs')
+     .then((response) => response.json())
+     .then((data) => console.log(data));
+     showData.innerHTML = ""
+}
