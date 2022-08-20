d9ab0dba51e900b898f644b4a11d513970120576
abdallamfathy <abdallamfathy@gmail.com>
2022-08-20 14:38:23 +0200

    setting variables

---
+++ main.js
@@ -0,0 +1,6 @@
+// Setting variables
+let inputData = document.querySelector(".get-repos input");
+let getButton = document.querySelector(".get-button");
+let showData = document.querySelector(".show-data span");
+let showDataFetched = document.querySelector(".show-data");
+
