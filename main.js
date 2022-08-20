c04662986b973ff657d3839e217096c259daebcf
abdallamfathy <abdallamfathy@gmail.com>
2022-08-20 15:16:21 +0200

    Display repos and make links for it

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 let inputData = document.querySelector(".get-repos input");
 let getButton = document.querySelector(".get-button");
 let showData = document.querySelector(".show-data span");
-let showDataFetched = document.querySelector(".show-data");
+let reposData = document.querySelector(".show-data");
 
 getButton.onclick = function () {
     if (inputData.value === '') {
         showData.innerHTML = "Please Enter repo's username!"
     } else {
         getData();
-        
+        showData.innerHTML = ""
          
     }
 }
@@ -17,6 +17,26 @@ getButton.onclick = function () {
 function getData() {
       fetch('https://api.github.com/users/hadley/orgs')
      .then((response) => response.json())
-     .then((data) => console.log(data));
-     showData.innerHTML = ""
+     .then((repos) => {
+        repos.forEach(repo => {
+        // Create div
+        let mainDiv = document.createElement("div");
+        // Create text
+        let repoName = document.createTextNode(repo.name);
+        // Add text to mainDiv
+        mainDiv.append(repoName);
+        // Create Anchor tag
+        let link = document.createElement("a");
+        // Add text to tag
+        let linkText = document.createTextNode("visit");
+        // Add text to link
+        link.appendChild(linkText);
+        // Add href to link
+        link.href = `https://github.com/${inputData.value}/${repo.name}`;
+        // Add link to mainDiv
+        mainDiv.appendChild(link);
+        });
+        
+     });
+     
 }
